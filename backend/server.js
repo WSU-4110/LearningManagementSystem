b153d46d1f453ba3e04a24a5049a2aeb526f3d2d
David <david_pogosian@outlook.com>
2023-09-25 19:38:13 -0400

    server runs & connects to atlas

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
-const port = 5000;
-
+const port = process.env.PORT || 5000;
+const atlasURI = process.env.ATLAS_URI;
+mongoose.connect(atlasURI, { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+    console.log('atlas connected')
+});
 app.listen(port, () => {
-    console.log(`server running on port ${port}`)
+    console.log(`server running on port ${port}`);
 });
