0850e3f3ac4855ad6d44fca2f0523c9b6622ebff
David <david_pogosian@outlook.com>
2023-09-25 16:25:37 -0400

    adopted 2 folder structure (front & back)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,8 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Learning Management System
+          Edit <code>src/App.js</code> and save to reload.
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
