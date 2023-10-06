38982c8409b1736b52f912c41f7f335200cd0680
David <david_pogosian@outlook.com>
2023-10-05 20:22:27 -0400

    disabled navbar, added assignment component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 
 
-import Navbar from './components/navbar.component';
+//import Navbar from './components/navbar.component';
 import Dashboard from './components/dashboard.component';
 import Course from './components/course.component';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Signin from './components/signin.component';
+import Assignment from './components/assignment.component';
 
 function App() 
 {
@@ -13,12 +14,11 @@ function App()
     
 
     <Router>
-      <Navbar />
       <Routes>
         <Route path = '/' element={<Dashboard/>} />
         <Route path = '/course' element={<Course/>} />
         <Route path = '/signin' element={<Signin/>} />
-
+        <Route path = '/assignment' element={<Assignment/>} />
       </Routes>
     </Router>
   );
