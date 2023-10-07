3b1b407e4cb4ab85a292dd55eafcc0548ac46bd1
David <david_pogosian@outlook.com>
2023-10-07 17:31:10 -0400

    messing with dashbord

--- frontend/src/components/dashboard.component.js
+++ frontend/src/components/dashboard.component.js
@@ -6,13 +6,24 @@ function CoursePeak(course) {
         <h1>{Object.values(course)}</h1>
     );
 };
+function ihatejs(obj) {
+    console.log(`${obj} is of type: ${typeof(obj)}`);
+    if (typeof(obj) === 'object') {
+        console.log(`${obj} has keys: ${Object.keys(obj)}`);
+        for (let i = 0; i < Object.keys(obj).length; i++) {
+            console.log(`going into key: ${Object.keys(obj)[i]}`);
+            ihatejs(obj[Object.keys(obj)[i]]);
+        }
+    }
+}
 export default function Dashboard() {
     const { id } = useParams();
     const [courses, setCourses] = useState([]);
     useEffect(() => {
         axios.get('http://localhost:5050/students/getCourses/' + id)
             .then(courses => {
-                setCourses(courses.data);
+                setCourses(["str1", "str2"]);
+                ihatejs(courses.data);
             })
             .catch(err => { console.log('error: ' + err) });
     }, []);
