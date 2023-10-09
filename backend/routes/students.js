cc7511928f39bb759b43312c083c2d08966423b0
David <david_pogosian@outlook.com>
2023-10-09 15:19:15 -0400

    + firstName & lastName to student schema

--- backend/routes/students.js
+++ backend/routes/students.js
@@ -9,15 +9,27 @@ router.route('/').get(async (req, res) => {
         res.status(400).json('error: ' + err);
     }
 });
+router.route('/:id').get(async (req, res) => {
+    try {
+        let student = await Student.findById(req.params.id);
+        res.json(student);
+    } catch(err) {
+        res.status(400).json('error: ' + err);
+    }
+});
 router.route('/add').post((req, res) => {
-    const email = req.body.email; // fyi: app must use(express.json()) to read like this
-    const password = req.body.password;
     const salt = randomBytes(128);
-    scrypt(password, salt, 256, (err, derivedKey) => {
+    scrypt(req.body.password, salt, 256, (err, derivedKey) => {
         if (err) throw err;
         const hash = derivedKey.toString('base64');
         const stringSalt = salt.toString('base64');
-        const newStudent = Student({ email: email, password: hash, salt: stringSalt });
+        const newStudent = Student({ 
+            email: req.body.email, 
+            password: hash, 
+            salt: stringSalt,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName
+        });
         newStudent.save()
             .then(() => res.json('student added!'))
             .catch(err => res.status(400).json('error: ' + err));
