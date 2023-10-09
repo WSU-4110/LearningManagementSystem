e0929eb765316190773fcd03c67049f0b5d7bcf5
Rei Pullum <rei@pullum.com>
2023-10-08 23:36:04 -0400

    started on basics for course.components

--- frontend/src/components/course.component.js
+++ frontend/src/components/course.component.js
@@ -1,8 +1,37 @@
 import { Link } from 'react-router-dom';
+import React, {useState, useEffect} from 'react'
 export default function Course() {
+
+    {/*courseTextDisplay is used to change the font for the header "Courses"*/}
+    const courseTextDisplay = 
+    {
+        color: '#F67280',
+        textDecoration: 'underline',
+    };
+
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() => 
+    {
+        fetch('/courses')
+        .then((response) => response.json())
+        .then((data) => setCourses(data))
+    });
+    
     return(
         <div>
-            <p>this is a course</p>
+            <h1 style={courseTextDisplay}>Courses</h1>
+            <ul>
+
+            {/*To be continued by Rei to print actual students courses*/}
+            {courses.map((course) => 
+                (
+                    <li key={course._id}>{course.name}</li>
+                ))}
+                <li><Link to="/course">course1</Link></li>
+                <li><Link to="/course">course2</Link></li>
+                <li><Link to="/course">course3</Link></li>
+            </ul>
             <Link to="/assignment">assignment</Link>
         </div>
     );
