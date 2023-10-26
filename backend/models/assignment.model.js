7a5c23a1c012e54adddefd8f30b725c38c42c277
jrule5960 <gq2277@wayne.edu>
2023-10-26 16:02:38 -0400

    Added design pattern to assignment.model.js for homework 4

--- backend/models/assignment.model.js
+++ backend/models/assignment.model.js
@@ -6,3 +6,69 @@ const assignmentSchema = mongoose.Schema({
 });
 const Assignment = mongoose.model('Assignment', assignmentSchema);
 module.exports = Assignment;
+
+
+class AssignmentService {
+    static instance = null;
+
+    constructor() {
+        if (!AssignmentService.instance) {
+            AssignmentService.instance = this;
+        }
+        return AssignmentService.instance;
+    }
+
+    // Other methods...
+}
+
+export default new AssignmentService();
+
+
+// CourseContainer.js
+import React, { Component } from 'react';
+import Course from './Course';
+import AssignmentService from './AssignmentService';
+
+class CourseContainer extends CourseComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            assignments: [],
+        };
+    }
+
+    componentDidMount() {
+        const { id } = this.props.match.params;
+        AssignmentService.getAssignments(id)
+            .then(assignmentList => {
+                this.setState({ assignments: assignmentList });
+            })
+            .catch(err => {
+                console.log('error: ' + err);
+            });
+    }
+
+    render() {
+        return <Course assignments={this.state.assignments} />;
+    }
+}
+
+//export default CourseContainer;
+
+// Course.js (Presentation Component)
+import React from 'react';
+import AssignmentPeak from './AssignmentPeak';
+
+const Course = ({ assignments }) => (
+    <div>
+        {assignments.map(assignment => (
+            <AssignmentPeak
+                assignmentName={assignment.name}
+                assignmentId={assignment._id}
+                key={assignment._id}
+            />
+        ))}
+    </div>
+);
+
+//export default Course;
