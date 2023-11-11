c7f63e5e08512a3207aba20b21a5f515163431e6
Kidd <kiddochang02@gmail.com>
2023-11-11 02:57:34 -0500

    Create new navbar
    Deleted old navbar completely and used a new react-bootstrap navbar, that's responsive and includes the same links. Also added header.css to finalize styling.

--- frontend/src/components/Header/Header.component.js
+++ frontend/src/components/Header/Header.component.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../../css/header.css';
 import http from '../../http';
 
 import Container from 'react-bootstrap/Container';
@@ -24,50 +25,24 @@ async function handleLogout() {
 function Header() {
   return (
       <>
-        {[false, 'sm', 'md', 'lg', 'xl', 'xxl'].map((expand) => (
-          <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
-            <Container fluid>
-              <Navbar.Brand as={Link} to="/dashboard">LMS</Navbar.Brand>
-              <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-              <Navbar.Offcanvas
-                id={`offcanvasNavbar-expand-${expand}`}
-                aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-                placement="end"
-              >
-                <Offcanvas.Header closeButton>
-                  <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                    LMS
-                  </Offcanvas.Title>
-                </Offcanvas.Header>
-                <Offcanvas.Body>
-                  <Nav className="justify-content-end flex-grow-1 pe-3">
-                    <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
-                    <NavDropdown
-                      title="User"
-                      id={`offcanvasNavbarDropdown-expand-${expand}`}>
-                      <NavDropdown.Item as={Link} to="/profilePage">Profile</NavDropdown.Item>
-                      <NavDropdown.Divider />
-                      <NavDropdown.Item
-                          onClick={handleLogout}
-                      >
-                        Log out
-                      </NavDropdown.Item>
-                    </NavDropdown>
-                  </Nav>
-                  <Form className="d-flex">
-                    <Form.Control
-                      type="search"
-                      placeholder="Search"
-                      className="me-2"
-                      aria-label="Search"
-                    />
-                    <Button variant="outline-success">Search</Button>
-                  </Form>
-                </Offcanvas.Body>
-              </Navbar.Offcanvas>
-            </Container>
-          </Navbar>
-        ))}
+        <Navbar expand="lg" className="bg-body-tertiary nav">
+          <Container className="nav-content">
+            <Navbar.Brand href="#home">LMS</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
+                <NavDropdown title="User" id="basic-nav-dropdown">
+                  <NavDropdown.Item as={Link} to="/profilePage">Profile</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item onClick={handleLogout}>
+                    Log out
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       <div>
         {/* 
         <Routes>
