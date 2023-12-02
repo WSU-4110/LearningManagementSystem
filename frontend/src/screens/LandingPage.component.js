7327d5c37d18fac07f24df921c6edcc25a6bb566
Adam Ahmed <adam.m.ahmeddm@gmail.com>
2023-12-02 18:15:05 -0500

    Navbar Functionality & Screen Sizing Fix

--- frontend/src/screens/LandingPage.component.js
+++ frontend/src/screens/LandingPage.component.js
@@ -1,28 +1,28 @@
 import React from 'react'
-import Container from 'react-bootstrap/esm/Container'
-import Row from 'react-bootstrap/esm/Row'
 import "./LandingPage.css"
 import {useNavigate} from 'react-router-dom';
 import Button from 'react-bootstrap/esm/Button'
+import { Link } from 'react-router-dom';
+import logoImage from '../css/logoTransp.png'
+// import Container from 'react-bootstrap/esm/Container'
+// import Row from 'react-bootstrap/esm/Row'
+
 
 const LandingPage = () => {
 	const navigate = useNavigate();
 	return (
 		<div className="container">
-			{/* <Container> */}
-				{/* <Row> */}
-					<div className="intro-text">
-						<div>
-							<h1 className="title">Welcome to your LMS</h1>
-							<p className="subtitle"></p>
-						</div>
-						<div className="buttonContainer">
-							<Button size="xl" onClick={() => {navigate("/studentLogin")}} className="landingButton" variant="outline-primary">I am a Student</Button>
-							<Button size="xl" onClick={() => {navigate('/instructorLogin')}} className="landingButton" variant="outline-primary">I am an Instructor</Button>
-						</div>
-					</div>
-				{/* </Row> */}
-			{/* </Container> */}
+			<div className="intro-text">
+				<div className='landing-logo'>
+                    <Link to="/">
+                        <img src={logoImage} alt="Logo" className="logo-image" />
+                    </Link>
+                </div>
+				<div className="buttonContainer">
+					<Button size="xl" onClick={() => {navigate("/studentLogin")}} className="landingButton" variant="outline-primary">I am a Student</Button>
+					<Button size="xl" onClick={() => {navigate('/instructorLogin')}} className="landingButton" variant="outline-primary">I am an Instructor</Button>
+				</div>
+			</div>
 		</div>
 	)
 }
