f5b3d74e8bc0a49c7301f871aaa826213216ed96
Adam Ahmed <adam.m.ahmeddm@gmail.com>
2023-12-05 09:20:52 -0500

    added functionality for instructors in profilePage

--- frontend/src/components/profilePage.component.js
+++ frontend/src/components/profilePage.component.js
@@ -9,21 +9,43 @@ export default function ProfilePage() {
     const [new_last_name, setNewLastName] = useState('');
     const [new_email, setNewEmail] = useState('');
     const [updateMessage, setUpdateMessage] = useState('');
+    const [instructorFlag, setInstructorFlag] = useState(false);
 
     useEffect(() => {
         async function getStudent() {
             try {
                 let response = await http.get(DATA_SERVER_URL + "/students/" + "_id");
                 let student_id = response.data._id;
-                response = await http.get(DATA_SERVER_URL + "/students/" + student_id);
-                setStudent(response.data.student);
-                setNewFirstName(response.data.student.firstName);
-                setNewLastName(response.data.student.lastName);
-                setNewEmail(response.data.student.email);
+                // console.log(response.data.firstName);
+                
+                if (response.data.firstName) {
+                    // console.log("student found");
+                    response = await http.get(DATA_SERVER_URL + "/students/" + student_id);
+                    setStudent(response.data.student);
+                    setNewFirstName(response.data.student.firstName);
+                    setNewLastName(response.data.student.lastName);
+                    setNewEmail(response.data.student.email);
+                } else {
+                    // console.log("instructor found.");
+                    response = await http.get(DATA_SERVER_URL + "/instructors/" + "_id");
+                    let instructor_id = response.data._id;
+                    
+                    if (instructor_id) {
+                        setInstructorFlag(true);
+                        response = await http.get(DATA_SERVER_URL + "/instructors/" + instructor_id);
+                        setStudent(response.data.instructor);
+                        setNewFirstName(response.data.instructor.firstName);
+                        setNewLastName(response.data.instructor.lastName);
+                        setNewEmail(response.data.instructor.email);
+                    } else {
+                        console.log("No user found");
+                    }
+                }
             } catch (error) {
-                console.log("Error fetching student", error);
+                console.log("Error fetching data", error);
             }
         }
+        
         getStudent();
     }, []);
 
@@ -31,18 +53,32 @@ export default function ProfilePage() {
         try {
             let response = await http.get(DATA_SERVER_URL + "/students/" + student._id);
             let student_obj = response.data.student;
+            if (student_obj){
+                student_obj.firstName = new_first_name;
+                student_obj.lastName = new_last_name;
+                student_obj.email = new_email;
+                console.log(student_obj);
 
-            student_obj.firstName = new_first_name;
-            student_obj.lastName = new_last_name;
-            student_obj.email = new_email;
-            console.log(student_obj);
+                await http.patch(DATA_SERVER_URL + "/students/", {student: student_obj });
+                setUpdateMessage('Profile updated successfully'); // Set the success message
+                setTimeout(() => setUpdateMessage(''), 3000);
+            } else if (instructorFlag) {
+                let response = await http.get(DATA_SERVER_URL + "/instructors/" + student._id);
+                let student_obj = response.data.instructor;
+                student_obj.firstName = new_first_name;
+                student_obj.lastName = new_last_name;
+                student_obj.email = new_email;
+                console.log(student_obj);
 
-            await http.patch(DATA_SERVER_URL + "/students/", {student: student_obj });
-            setUpdateMessage('Profile updated successfully'); // Set the success message
-            setTimeout(() => setUpdateMessage(''), 3000);
+                await http.patch(DATA_SERVER_URL + "/instructors/", {instructor: student_obj });
+                setUpdateMessage('Profile updated successfully'); // Set the success message
+                setTimeout(() => setUpdateMessage(''), 3000);
+            }
+
+
             //window.location.reload();
         } catch (error) {
-            console.error("Error updating student", error);
+            console.error("Error updating user", error);
         }
     }
 
