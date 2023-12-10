7749a237180624d225425d1dc714279fb4142b36
Jacob Rule <144157646+jrule5960@users.noreply.github.com>
2023-12-09 21:15:21 -0500

    Create instructorNewAssignment.test.js

---
+++ frontend/src/components/instructorComponents/instructorNewAssignment.test.js
@@ -0,0 +1,82 @@
+// Function for instructor to make an assignment
+function makeAssignment(instructorId, assignmentDetails) {
+  if (!instructorId || !assignmentDetails) {
+    throw new Error('Instructor ID and assignment details are required');
+  }
+
+  return `Assignment created by instructor ${instructorId}: ${assignmentDetails}`;
+}
+
+// Tests for the makeAssignment function
+describe('makeAssignment function', () => {
+  test('successfully makes an assignment', () => {
+    const instructorId = 456;
+    const assignmentDetails = 'Create a report on the assigned topic';
+
+    const result = makeAssignment(instructorId, assignmentDetails);
+
+    expect(result).toEqual(`Assignment created by instructor ${instructorId}: ${assignmentDetails}`);
+  });
+
+  test('throws an error if instructorId or assignmentDetails is missing', () => {
+    expect(() => makeAssignment(undefined, 'Create a report')).toThrow('Instructor ID and assignment details are required');
+
+    expect(() => makeAssignment(456, undefined)).toThrow('Instructor ID and assignment details are required');
+
+    expect(() => makeAssignment(undefined, undefined)).toThrow('Instructor ID and assignment details are required');
+  });
+
+  test('throws an error if instructorId is not provided', () => {
+    expect(() => {
+      makeAssignment(undefined, 'Create a report');
+    }).toThrowError('Instructor ID and assignment details are required');
+  });
+
+  test('throws an error if assignmentDetails is not provided', () => {
+    expect(() => {
+      makeAssignment(456, undefined);
+    }).toThrowError('Instructor ID and assignment details are required');
+  });
+
+  test('throws an error if both instructorId and assignmentDetails are not provided', () => {
+    expect(() => {
+      makeAssignment(undefined, undefined);
+    }).toThrowError('Instructor ID and assignment details are required');
+  });
+
+  test('successfully makes an assignment with complex details', () => {
+    const instructorId = 789;
+    const assignmentDetails = 'Write a program to solve a given algorithmic problem. Due in 2 weeks.';
+
+    const result = makeAssignment(instructorId, assignmentDetails);
+
+    expect(result).toEqual(`Assignment created by instructor ${instructorId}: ${assignmentDetails}`);
+  });
+
+  test('successfully makes an assignment with special characters in details', () => {
+    const instructorId = 123;
+    const assignmentDetails = 'Create a presentation on "The History of Computer Science"';
+
+    const result = makeAssignment(instructorId, assignmentDetails);
+
+    expect(result).toEqual(`Assignment created by instructor ${instructorId}: ${assignmentDetails}`);
+  });
+
+  test('successfully makes an assignment with a long description', () => {
+    const instructorId = 789;
+    const assignmentDetails = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+    const result = makeAssignment(instructorId, assignmentDetails);
+
+    expect(result).toEqual(`Assignment created by instructor ${instructorId}: ${assignmentDetails}`);
+  });
+
+  test('successfully makes an assignment with empty assignmentDetails', () => {
+    const instructorId = 123;
+    const assignmentDetails = '';
+
+    const result = makeAssignment(instructorId, assignmentDetails);
+
+    expect(result).toEqual(`Assignment created by instructor ${instructorId}: ${assignmentDetails}`);
+  });
+});
