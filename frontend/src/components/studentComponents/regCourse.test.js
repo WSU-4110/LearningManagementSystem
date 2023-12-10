d9f1867ddf4bed1bd76ab65fb0ee9c56fcd8fdd8
Jacob Rule <144157646+jrule5960@users.noreply.github.com>
2023-12-09 20:56:25 -0500

    Create regCourse.test.js

---
+++ frontend/src/components/studentComponents/regCourse.test.js
@@ -0,0 +1,103 @@
+function RegisterCourse({ onRegister }) {
+  const [courseName, setCourseName] = React.useState('');
+  const [registeredCourses, setRegisteredCourses] = React.useState([]);
+  const [error, setError] = React.useState(null);
+
+  const handleRegister = () => {
+    if (courseName.trim() === '') {
+      setError('Course name is required');
+      return;
+    }
+
+    // Simulating an API call to register for a course
+    // In a real application, this would be an HTTP request
+    onRegister(courseName)
+      .then(() => {
+        setRegisteredCourses([...registeredCourses, courseName]);
+        setCourseName('');
+        setError(null);
+      })
+      .catch((err) => {
+        setError('Failed to register for the course');
+      });
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={courseName}
+        onChange={(e) => setCourseName(e.target.value)}
+        placeholder="Enter course name"
+        data-testid="course-input"
+      />
+      <button onClick={handleRegister} data-testid="register-button">
+        Register
+      </button>
+      {error && <p role="alert">{error}</p>}
+      <div data-testid="registered-courses">
+        <h3>Registered Courses:</h3>
+        <ul>
+          {registeredCourses.map((course, index) => (
+            <li key={index}>{course}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+// Jest tests for RegisterCourse component
+describe('RegisterCourse component', () => {
+  test('registers a course successfully', async () => {
+    const mockRegister = jest.fn().mockResolvedValueOnce();
+
+    render(<RegisterCourse onRegister={mockRegister} />);
+
+    const courseInput = screen.getByTestId('course-input');
+    const registerButton = screen.getByTestId('register-button');
+
+    fireEvent.change(courseInput, { target: { value: 'Mathematics' } });
+    fireEvent.click(registerButton);
+
+    await waitFor(() => {
+      expect(mockRegister).toHaveBeenCalledWith('Mathematics');
+      const registeredCourse = screen.getByText('Mathematics');
+      expect(registeredCourse).toBeInTheDocument();
+    });
+  });
+
+  test('handles registration with empty course name', async () => {
+    const mockRegister = jest.fn();
+
+    render(<RegisterCourse onRegister={mockRegister} />);
+
+    const registerButton = screen.getByTestId('register-button');
+
+    fireEvent.click(registerButton);
+
+    const errorElement = await screen.findByRole('alert');
+    expect(errorElement).toHaveTextContent('Course name is required');
+    expect(mockRegister).not.toHaveBeenCalled();
+  });
+
+  test('handles error during course registration', async () => {
+    const mockRegister = jest.fn().mockRejectedValueOnce(new Error('Failed to register'));
+
+    render(<RegisterCourse onRegister={mockRegister} />);
+
+    const courseInput = screen.getByTestId('course-input');
+    const registerButton = screen.getByTestId('register-button');
+
+    fireEvent.change(courseInput, { target: { value: 'Physics' } });
+    fireEvent.click(registerButton);
+
+    await waitFor(() => {
+      expect(mockRegister).toHaveBeenCalledWith('Physics');
+      const errorElement = screen.getByRole('alert');
+      expect(errorElement).toHaveTextContent('Failed to register for the course');
+    });
+  });
+
+  // Add more tests for edge cases and different scenarios
+});
